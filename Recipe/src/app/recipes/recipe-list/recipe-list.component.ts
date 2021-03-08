a816e2a9a93f0205fcb76eb44ad86d03550fe235
kashuvo <shuvo_CSE23@yahoo.com>
2021-03-08 20:19:08 +0600

    Added Service with DI

--- Recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,19 +8,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg'),
-    new Recipe('A Test Recipe 2', 'This is simply a test', 'https://www.topsrilankanrecipe.com/wp-content/uploads/2019/04/21a.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ Recipe/src/app/recipes/recipe.service.ts
@@ -0,0 +1,37 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('Street Shomocha',
+            'Oil fried shomocha, popular in asian countries',
+            'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-480x270.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French fries', 20)
+            ]),
+        new Recipe(
+            'Street Shingara',
+            'Fried in oil and used potato and vegitables',
+            'https://www.topsrilankanrecipe.com/wp-content/uploads/2019/04/21a.jpg',
+            [
+                new Ingredient('Ata', 1),
+                new Ingredient('Potato', 50)
+            ])
+    ];
+
+    constructor(private slService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+}
