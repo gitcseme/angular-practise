f1d0413d1c8dbb6783d80dffc3496ac549e15e4a
kashuvo <shuvo_CSE23@yahoo.com>
2021-04-15 22:48:12 +0600

    added HTTP Interceptor for attaching token with request

--- Recipe/src/app/shared/data-storage.service.ts
+++ Recipe/src/app/shared/data-storage.service.ts
@@ -19,19 +19,12 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(
-            take(1), 
-            exhaustMap(user => {
-                return this.httpClient.get<Recipe[]>(
-                    'https://recipe-book-87ca3-default-rtdb.firebaseio.com/recipes.json',
-                    {
-                        params: new HttpParams().set('auth', user.token)
-                    }
-                );
-            }), 
+        return this.httpClient
+        .get<Recipe[]>('https://recipe-book-87ca3-default-rtdb.firebaseio.com/recipes.json',)
+        .pipe(
             map(recipes => {
                 return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
                 });
             }),
             tap(recipes => {
