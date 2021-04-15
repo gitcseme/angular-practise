9eee3a88651280831b3121dc35ad951d526bf120
kashuvo <shuvo_CSE23@yahoo.com>
2021-04-16 01:01:36 +0600

    Added Guard to protect route

--- Recipe/src/app/app-routing.module.ts
+++ Recipe/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuardService } from "./auth/auth-guard.service";
 import { AuthComponent } from "./auth/auth.component";
 
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
@@ -11,7 +12,9 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent, children: [
+    { path: 'recipes', component: RecipesComponent, 
+      canActivate: [AuthGuardService],
+      children: [
         { path: '', component: RecipeStartComponent },
         { path: 'new', component: RecipeEditComponent },
         { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService] },
