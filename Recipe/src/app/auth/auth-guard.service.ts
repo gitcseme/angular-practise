32d20c9f5fe49ad3bfbb8cf0d76aa93bb2699ebb
kashuvo <shuvo_CSE23@yahoo.com>
2021-04-19 00:43:08 +0600

    alert error with ngIf

--- Recipe/src/app/auth/auth-guard.service.ts
+++ Recipe/src/app/auth/auth-guard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, take } from "rxjs/operators";
 import { AuthService } from "./auth.service";
 
 @Injectable({ providedIn: 'root' })
@@ -9,7 +9,9 @@ export class AuthGuardService  implements CanActivate {
     constructor(private authService: AuthService, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.authService.user.pipe(map(user => {
+        return this.authService.user.pipe(
+            take(1),
+            map(user => {
             const isAuthenticated = !!user;
             if (isAuthenticated) {
                 return true;
