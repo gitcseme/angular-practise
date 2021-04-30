3bd5806e13f3d3eae84e29040e37daac18cd33b5
kashuvo <shuvo_CSE23@yahoo.com>
2021-04-30 10:45:45 +0600

    Recipes routing are separated

--- Recipe/src/app/recipes/recipes.module.ts
+++ Recipe/src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 import { RecipesComponent } from "./recipes.component";
 
 @NgModule({
@@ -22,15 +23,16 @@ import { RecipesComponent } from "./recipes.component";
     imports: [
         RouterModule,
         CommonModule,
-        ReactiveFormsModule
-    ],
-    exports: [
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
-        RecipeStartComponent,
-        RecipeEditComponent
+        ReactiveFormsModule,
+        RecipesRoutingModule
     ]
+    // exports: [
+    //     RecipesComponent,
+    //     RecipeListComponent,
+    //     RecipeDetailComponent,
+    //     RecipeItemComponent,
+    //     RecipeStartComponent,
+    //     RecipeEditComponent
+    // ]
 })
 export class RecipesModule {}
